ca29f90e03bf15b8753dee8399950717477411a6
Dominik Woźniak <dominikwozniak@MacBook-Pro-Hatimeria.local>
2021-05-29 18:55:27 +0200

    Custom error handlers

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map(({msg, param}) => {
+      return {
+        message: msg,
+        field: param
+      }
+    });
+    res.status(400).send({
+      errors: formattedErrors
+    });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({
+      errors: [
+        {
+          message: err.reason
+        }
+      ]
+    });
+  }
+
+  res.status(400).send({
+    errors: [
+      {
+        message: 'Something went wrong'
+      }
+    ]
+  });
+};
