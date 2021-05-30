119f1564181458fbd64e684dc51d3233467d83f1
Dominik Woźniak <dominikwozniak@MacBook-Pro-Hatimeria.local>
2021-05-30 11:39:30 +0200

    Add not-found-error route handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,10 +2,11 @@ import express  from 'express';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from "./routes/signin";
-import { signupRouter } from "./routes/signup";
-import { signoutRouter } from "./routes/signout";
-import { errorHandler } from "./middlewares/error-handler";
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
@@ -15,6 +16,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.all('*', () => {
+  throw new NotFoundError()
+})
 
 app.use(errorHandler);
 
