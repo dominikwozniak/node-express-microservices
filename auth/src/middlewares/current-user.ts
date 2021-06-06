571c9a44ffa49feddc33d7917df27f1b8a2f241a
Dominik Woźniak <dominikwozniak@MacBook-Pro-Hatimeria.local>
2021-06-06 19:31:29 +0200

    Add require-auth middleware

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -31,4 +31,4 @@ export const currentUser = (
   } catch (err) {}
 
   next();
-}
+};
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response } from 'express';
+import { requireAuth } from '../middlewares/require-auth';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.post('/api/users/signout', (req: Request, res: Response) => {
+router.post(
+  '/api/users/signout',
+  currentUser,
+  requireAuth,
+  (req: Request, res: Response) => {
   req.session = null;
 
   res.send({});
