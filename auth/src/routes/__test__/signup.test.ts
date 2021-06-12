e4fd3721a375ed7b453db5beee75a55734d0392a
Dominik Woźniak <dominikwozniak@MacBook-Pro-Hatimeria.local>
2021-06-12 15:34:29 +0200

    Signup tests

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,71 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '12345678'
+    })
+    .expect(201);
+});
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test',
+      password: '12345678'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '1'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com'
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: '12345678'
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send()
+    .expect(400);
+});
+
+it('disallows duplicate email addresses', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '12345678'
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '12345678'
+    })
+    .expect(400);
+});
