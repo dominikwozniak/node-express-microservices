76b47427cf2f3cd0cd6d595517ad9b8a372bdf77
Dominik Woźniak <dominikwozniak@MacBook-Pro-Hatimeria.local>
2021-06-12 16:40:11 +0200

    Current-user tests

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,27 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+let cookie: string[];
+
+beforeEach(async () => {
+  const res = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '12345678'
+    })
+    .expect(201);
+
+  cookie = res.get('Set-Cookie');
+});
+
+it('responds with details about the current user', async () => {
+  const res = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
+
+  expect(res.body.currentUser.email)
+    .toEqual('test@test.com')
+});
