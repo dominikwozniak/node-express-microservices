05312145fb3fbc758a602a0ff28c101a52441951
Dominik Woźniak <dominikwozniak@MacBook-Pro-Hatimeria.local>
2021-06-19 16:02:55 +0200

    Validate new ticket request

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,9 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { errorHandler, NotFoundError } from '@dwticketing/common';
+import { errorHandler, NotFoundError, currentUser } from '@dwticketing/common';
+
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 
@@ -13,6 +15,10 @@ app.use(cookieSession({
     secure: process.env.NODE_ENV !== 'test'
   })
 );
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,19 +2,65 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('has a route handler listening to /api/tickets for post request', async () => {
+  const res = await request(app)
+    .post('/api/tickets')
+    .send({});
 
+  expect(res.status).not.toEqual(404);
 });
 
 it('can only be accessed if the user is signed in', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .send({})
+    .expect(401);
+});
 
+it('returns a status other than 401 if the user is signed in', async () => {
+  const res = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({});
+
+  expect(res.status).not.toEqual(401);
 });
 
 it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 2137
+    })
+    .expect(400);
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 2137
+    })
+    .expect(400);
 });
 
 it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'test1234',
+      price: -10
+    })
+    .expect(400);
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'test',
+    })
+    .expect(400);
 });
 
 it('creates a ticket with valid inputs', async () => {
