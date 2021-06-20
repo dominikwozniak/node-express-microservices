671d781b157ba9959c21a666c9dde54a42df05e4
Dominik Woźniak <dominikwozniak@MacBook-Pro-Hatimeria.local>
2021-06-20 16:00:00 +0200

    Get ticket by id

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@dwticketing/common';
 
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(cookieSession({
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
