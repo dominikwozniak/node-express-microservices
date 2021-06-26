871f764644f1d05e378acf3fe1d62a5cbb3dc7ed
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-06-26 14:17:39 +0200

    Listener basic implementation

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -6,6 +6,7 @@ Before run
 >> kubectl get pods
 - find nats-depl name
 >> kubectl port-forward nats-depl-xxx 4222:4222
+>> kubectl port-forward nats-depl-xxx 8222:8222
 */
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
@@ -15,7 +16,21 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+  stan.on('close', () => {
+    console.log('NATS connection closed');
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions()
+      .setManualAckMode(true)
+      .setDeliverAllAvailable()
+      .setDurableName('accounting-service');
+
+  const subscription = stan.subscribe(
+      'ticket:created',
+      'queue-group-name',
+      options
+  );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
@@ -23,5 +38,10 @@ stan.on('connect', () => {
     if (typeof data === 'string') {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
     }
+
+    msg.ack();
   });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on("SIGTERM", () => stan.close());
