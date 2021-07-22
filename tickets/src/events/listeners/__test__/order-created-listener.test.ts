876d3b564a999d9da15133e7f219d43f6484895f
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-07-22 21:20:41 +0200

    Add nats wrapper client to common

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -51,3 +51,11 @@ it('acks the message', async () => {
 
   expect(msg.ack).toHaveBeenCalled();
 });
+
+it('publishes a ticket updated event', async () => {
+  const { listener, ticket, data, msg } = await setupTest();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
